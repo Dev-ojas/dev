b251c5add552da29eb28df4c24dc17de991130e8
venkatgopidev <98175325+venkatgopidev@users.noreply.github.com>
2023-03-03 10:18:42 +0530

    var file added

---
+++ variables.tf
@@ -0,0 +1,25 @@
+# variable "x" {
+#     # default = "t2.micro"
+#     type = bool
+# }
+
+# variable "istest" {
+#   type = bool
+# }
+
+# variable "azaws" {
+#     default = ["ap-south-1a", "ap-south-1b","ap-south-1c"]  
+#     type = list
+# }
+
+variable "types"{
+    type = map
+    default = {
+        us-east-1 = "t2.nano",
+        ap-south-1 = "t2.micro",
+        us-west-1 = "t2.medium"
+        }
+}
+output "msg" {
+    value = var.types["us-east-1"]
+}
